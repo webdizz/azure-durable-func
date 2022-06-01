f17c917f520770f53824c414f581fecf94b63b32
Izzet Mustafaiev <izzet@mustafaiev.com>
2022-06-01 11:42:20 +0200

    Initial simple working example

---
+++ bulk-load-executor/index.ts
@@ -0,0 +1,12 @@
+﻿import { AzureFunction, Context } from "@azure/functions";
+
+const bulkLoaderFunction: AzureFunction = async function (
+  context: Context
+): Promise<string> {
+  context.log(
+    `#bulk-load-executor Is about to perform operation for ${context.bindings.name}`
+  );
+  return `Hello ${context.bindings.name}!`;
+};
+
+export default bulkLoaderFunction;
---
+++ bulk-load-orchestrator/index.ts
@@ -0,0 +1,21 @@
+﻿import * as df from "durable-functions";
+
+const orchestrator = df.orchestrator(function* (context) {
+  const outputs = [];
+
+  context.log(
+    "#bulk-load-orchestrator Is about to trigger first execution of bulk-load-executor"
+  );
+  outputs.push(
+    yield context.df.callActivity("bulk-load-executor", "product-family")
+  );
+  outputs.push(
+    yield context.df.callActivity("bulk-load-executor", "digital-taxonomy")
+  );
+
+  context.log(`#bulk-load-orchestrator got response from executors ${outputs}`);
+
+  return outputs;
+});
+
+export default orchestrator;
